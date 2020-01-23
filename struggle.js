72457f139aadf501217edb4155766f6b96b99322
Karl Hohn <kfhohn@gmail.com>
2020-01-23 00:41:20 -0500

    add sound effects

--- struggle.js
+++ struggle.js
@@ -6,10 +6,16 @@ const score2 = document.getElementById('score2');
 const resetBtn = document.getElementById('reset');
 const winState = document.getElementById('winState');
 const winnerField = document.getElementById('winner');
+const music = document.getElementById('music');
+const punch1 = document.getElementById('punch1');
+const punch2 = document.getElementById('punch2');
+const punch3 = document.getElementById('punch3');
+const bells = document.getElementById('bells');
 const hpMax = 10;
 
 var player1hp = hpMax,
-	player2hp = hpMax;
+	player2hp = hpMax,
+	hitCounter = 0;
 
 
 function incrementScore(event) {
@@ -22,7 +28,24 @@ function incrementScore(event) {
 	score1.innerHTML = player1hp;
 	score2.innerHTML = player2hp;
 
+	switch(hitCounter) {
+		case 0:
+			punch1.play();
+			break;
+		case 1:
+			punch2.play();
+			break;
+		case 2:
+			punch3.play();
+			hitCounter = -1;
+			break;
+	}
+
+	hitCounter++;
+
 	if (player1hp == 0 || player2hp == 0) {
+		music.pause();
+		bells.play();
 
 		for (var i=0, ii=goals.length; i<ii; i++) {
 			goals[i].classList.add('hidden');
@@ -37,7 +60,6 @@ function reset(event) {
 	event.preventDefault();
 
 	for (var i=0, ii=goals.length; i<ii; i++) {
-		console.log(goals);
 		goals[i].classList.remove('hidden');
 	}
 
@@ -49,6 +71,9 @@ function reset(event) {
 
 	player2hp = hpMax;
 	score2.innerHTML = player2hp;
+
+	music.currentTime = 0;
+	music.play();
 }
 
 resetBtn.addEventListener('mouseup', reset);
